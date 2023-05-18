5a92a7badc3e1c0042fe1830d49959d0107de342
Sarah_Harouni <60361890+saraharouni@users.noreply.github.com>
2023-05-18 16:11:07 +0200

    Update ui.py

--- dashboard/ui.py
+++ dashboard/ui.py
@@ -211,11 +211,12 @@ with col_graph:
         sns.move_legend(ax, loc="upper left", bbox_to_anchor=(1.1, 0.5))
         
     elif chart_type == 'Histogram':
-        df_train = df_train.replace([np.inf, -np.inf], np.nan, inplace=True)
-        ax = sns.histplot(data=df_train, x=x_variable, hue=hue_variable) #,multiple="stack",
+        #df_train = df_train.replace([np.inf, -np.inf], np.nan, inplace=True)
+        ax = sns.histplot(data=df_train, x=x_variable, hue=hue_variable) 
+        #,multiple="stack",
         #sns.move_legend(ax, loc="upper left", bbox_to_anchor=(1.1, 0.5))
-        ax.legend(loc="upper left", bbox_to_anchor=(1.1, 0.5))
-        plt.show()
+        #ax.legend(loc="upper left", bbox_to_anchor=(1.1, 0.5))
+        #plt.show()
     elif chart_type == 'Box':
         ax = sns.boxplot(data=df_train, x=x_variable, y=y_variable, hue=hue_variable)
         sns.move_legend(ax, loc="center left", bbox_to_anchor=(1.1, 0.5))
